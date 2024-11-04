db562546f1d1201261c987d44fb5fb090522d7e7
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2024-11-04 22:48:24 +0100

    welcome page

---
+++ magia_kawy/screens/WelcomeScreen.tsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
+
+const WelcomeScreen = () => {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://xxx.xxx.xxx.xxx:8082/auth/signin', {
+        email: email,
+        password: password,
+      });
+      console.log('Login success:', response.data);
+      Alert.alert('Success', 'Login successful!');
+      navigation.navigate('RejestracjaScreen');
+    } catch (error) {
+      console.error('Login failed:', error);
+      Alert.alert('Error', 'Login failed. Please check your credentials.');
+    }
+  };
+  
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/welcome-image.png')}
+        style={styles.image}
+      />
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>Email address</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="janek@example.com"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>Password</Text>
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={[styles.input, styles.passwordInput]}
+            placeholder="●●●●●●●●"
+            secureTextEntry={!passwordVisible}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity
+            style={styles.eyeIcon}
+            onPress={() => setPasswordVisible(!passwordVisible)}
+          >
+            <FontAwesome name={passwordVisible ? 'eye-slash' : 'eye'} size={24} color="#666" />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Log In</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.footerText}>
+          Don't have an account?{' '}
+          <Text style={styles.signupText} onPress={() => navigation.navigate('RejestracjaScreen')}>
+            Sign up
+          </Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 40,
+    left: 16,
+  },
+  backButtonText: {
+    fontSize: 24,
+    color: '#000',
+  },
+  image: {
+    width: 500,
+    height: 250,
+    marginBottom: 60,
+    resizeMode: 'contain',
+  },
+  formContainer: {
+    width: '100%',
+    backgroundColor: '#c9ad92', 
+    padding: 24,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+    color: '#333',
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    marginBottom: 16,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  passwordInput: {
+    flex: 1,
+    paddingRight: 40, 
+  },
+  eyeIcon: {
+    position: 'absolute',
+    right: 10,
+    top:8,
+  },
+  
+  loginButton: {
+    backgroundColor: '#FFA500', 
+    paddingVertical: 15,
+    borderRadius: 25,
+    alignItems: 'center',
+    marginVertical: 16,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  footerText: {
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#666',
+  },
+  signupText: {
+    color: '#007BFF', 
+    fontWeight: 'bold',
+  },
+});
+
+export default WelcomeScreen;
